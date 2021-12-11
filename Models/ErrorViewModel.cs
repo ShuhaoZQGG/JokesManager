640ba39da6992fbf2d7ca3bfa0a99711605ed2ff
shuha <shuhao.zhang@mail.mcgill.ca>
2021-12-11 12:18:28 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JokesManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
